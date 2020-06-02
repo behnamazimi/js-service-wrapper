4efdea1a8c00569a4995ddaa238e647143b3d74b
Behnam Azimi <behnam@spinmatic.com>
2020-06-02 16:01:24 +0300

    remove useless packages and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,6 @@ module.exports = {
                 exclude: /(node_modules)/,
                 use: 'babel-loader',
             },
-            // {
-            //     test: /\.css$/,
-            //     use: ["style-loader", "css-loader"]
-            // }
         ],
     },
     resolve: {
